d8a27235d9ff77e07beef558fb871da3e25148cc
mailovemisa <maiptn8399@gmail.com>
2025-05-28 17:03:01 +0700

    use Lua script to reduce round-trip and assure atomicity

--- social_todo_app_go/module/user/infras/repository/user_session_redis_repo.go
+++ social_todo_app_go/module/user/infras/repository/user_session_redis_repo.go
@@ -2,7 +2,7 @@ package repository
 
 import (
 	"context"
-	"github.com/go-redis/redis/v8"
+	"fmt"
 	"github.com/google/uuid"
 	"social_todo_app_go/module/user/domain"
 	"time"
@@ -27,6 +27,24 @@ func (r *sessionRedisRepo) Create(ctx context.Context, data *domain.UserSession)
 		RefreshExpAt: data.RefreshExpAt(),
 	}
 	key := "session:" + dto.Id.String()
+	r.client.Eval(ctx, `
+redis.call("HSET", KEYS[1],
+"id", ARGV[1],
+"user_id", ARGV[2],
+"refresh_token", ARGV[3],
+"access_exp_at", ARGV[4],
+"refresh_exp_at", ARGV[5]
+)
+redis.call("EXPIRE", KEYS[1], tonumber(ARGV[6]))
+redis.call("SET", KEYS[2], ARGV[1], "EX", tonumber(ARGV[7])
+return 1`,
+		[]string{key, "refresh:" + dto.RefreshToken}, []interface{}{
+			dto.Id.String(),
+			dto.UserId.String(),
+			dto.RefreshToken,
+			dto.AccessExpAt.Format("2006-01-02 15:04:05"),
+			dto.RefreshExpAt.Format("2006-01-02 15:04:05"),
+		})
 	err := r.client.HSet(ctx, key, map[string]interface{}{
 		"id":             dto.Id.String(),
 		"user_id":        dto.UserId.String(),
@@ -87,23 +105,36 @@ func (r *sessionRedisRepo) FindByRefreshToken(ctx context.Context, refreshToken 
 		return nil, err
 	}
 	session_key := "session:" + sessionId
-	data, err := r.client.HGetAll(ctx, session_key).Result()
+	data, err := r.client.Eval(ctx, `
+		local sessionId = redis.call('GET', KEYS[1])
+		if not sessionId then return nil end
+		return redis.call("HGETALL", "session:" .. sessionId)
+	`, []string{session_key}).Result()
 	if err != nil {
 		return nil, err
 	}
-	if len(data) == 0 {
+	if data == nil {
 		return nil, nil // Not found
 	}
-
-	id := uuid.MustParse(data["id"])
-	userId := uuid.MustParse(data["user_id"])
-	accessExpAt, _ := time.Parse("2006-01-02 15:04:05", data["access_exp_at"])
-	refreshExpAt, _ := time.Parse("2006-01-02 15:04:05", data["refresh_exp_at"])
+	data_arr, ok := data.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("session data type error")
+	}
+	result := make(map[string]string)
+	for i := 0; i < len(data_arr); i += 2 {
+		key := data_arr[i].(string)
+		value := data_arr[i+1].(string)
+		result[key] = value
+	}
+	id := uuid.MustParse(result["id"])
+	userId := uuid.MustParse(result["user_id"])
+	accessExpAt, _ := time.Parse("2006-01-02 15:04:05", result["access_exp_at"])
+	refreshExpAt, _ := time.Parse("2006-01-02 15:04:05", result["refresh_exp_at"])
 
 	return domain.NewUserSession(
 		id,
 		userId,
-		data["refresh_token"],
+		result["refresh_token"],
 		accessExpAt,
 		refreshExpAt,
 	), nil
